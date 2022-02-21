cd8964d894148f3ba85947e8a07e2db2c6ea2b11
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-02-21 19:18:36 -0300

    Layout base da tela de início

--- page-templates/index.php
+++ page-templates/index.php
@@ -1,3 +1,200 @@
-<main>
-  Página inicial
+<main style="margin-top: 25px;">
+  <div id="our-projects" class="block row g-0">
+    <div class="row">
+      <div class="col-12 col-xl-10 col-xxxl-8 offset-xl-1 offset-xxxl-2 px-4 px-sm-5 px-xl-0">
+        <div class="row justify-content-center">
+          <div class="col-lg-2 d-flex align-content-center">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+              <div class="col-12">
+                <div class="card">
+                  <!-- <img src="<?= __IMGDIR; ?>/header/logo.jpeg" class="card-img-top" alt="..."> -->
+                  <div class="card-body" style="background-color: #1A336D;">
+                    <h5 class="card-title p-4 text-center text-white">Tele-Educação</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card">
+                  <!-- <img src="<?= __IMGDIR; ?>/header/logo.jpeg" class="card-img-top" alt="..."> -->
+                  <div class="card-body " style="background-color: #C51A1B;">
+                    <h5 class="card-title p-4 text-center text-white">Tele Atendimento</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card">
+                  <!-- <img src="<?= __IMGDIR; ?>/header/logo.jpeg" class="card-img-top" alt="..."> -->
+                  <div class="card-body" style="background-color: #C48F27;">
+                    <h5 class="card-title p-4 text-center text-white">Tele Consultoria</h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="card">
+                  <!-- <img src="<?= __IMGDIR; ?>/header/logo.jpeg" class="card-img-top" alt="..."> -->
+                  <div class="card-body" style="background-color: #95C11F;">
+                    <h5 class="card-title p-4 text-center text-white">Tele Diagnóstico</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-10 mt-2">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                  aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                  aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                  aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="<?= __IMGDIR; ?>/img1.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Notícias</h5>
+                    <p>Fevereiro Roxo busca conscientizar sobre Lúpus, Fibromialgia e Alzheimer </p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="<?= __IMGDIR; ?>/img1.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Notícias</h5>
+                    <p>Fevereiro Roxo busca conscientizar sobre Lúpus, Fibromialgia e Alzheimer .</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="<?= __IMGDIR; ?>/img1.jpg" class="d-block w-100" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Notícias</h5>
+                    <p>Fevereiro Roxo busca conscientizar sobre Lúpus, Fibromialgia e Alzheimer </p>
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev">
+                <span sclass="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-xl-10 col-xxxl-8 offset-xl-1 offset-xxxl-2 pt-5 px-4 px-sm-5 px-xl-0">
+      <div class="row ">
+
+        <div class="col-lg-6">
+          <h2>Agenda e Eventos</h2>
+
+          <div class="card mb-3" style="max-width: 540px;">
+
+            <div class="row g-0">
+              <div class="col-md-2">
+                <span style="font-size: 16px;"
+                  class="badge bg-primary w-100 h-100 d-flex justify-content-center align-items-center">25/02</span>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Evento</h5>
+                  <p class="card-text">
+                    Web Palestra Saúde e TI
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 540px;">
+
+            <div class="row g-0">
+              <div class="col-md-2">
+                <span style="font-size: 16px;"
+                  class="badge bg-primary w-100 h-100 d-flex justify-content-center align-items-center">25/02</span>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Evento</h5>
+                  <p class="card-text">
+                    Web Palestra Saúde e TI
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mb-3" style="max-width: 540px;">
+
+            <div class="row g-0">
+              <div class="col-md-2">
+                <span style="font-size: 16px;"
+                  class="badge bg-primary w-100 h-100 d-flex justify-content-center align-items-center">25/02</span>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Evento</h5>
+                  <p class="card-text">
+                    Web Palestra Saúde e TI
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <h2>Ùltimas Notícias</h2>
+
+          <div class="row">
+            <div class="col">
+              <div class="card h-100">
+                <img src="<?= __IMGDIR; ?>/img1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Notícia</h5>
+                  <p class="card-text">
+                    Fevereiro Roxo busca conscientizar sobre Lúpus, Fibromialgia e Alzheimer
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Postado em 12/02/2022</small>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="<?= __IMGDIR; ?>/img1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Notícia</h5>
+                  <p class="card-text">
+                    Fevereiro Roxo busca conscientizar sobre Lúpus, Fibromialgia e Alzheimer
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Postado em 12/02/2022</small>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="<?= __IMGDIR; ?>/img1.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Notícia</h5>
+                  <p class="card-text">
+                    Fevereiro Roxo busca conscientizar sobre Lúpus, Fibromialgia e Alzheimer
+                  </p>
+                </div>
+                <div class="card-footer">
+                  <small class="text-muted">Postado em 12/02/2022</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </main><!-- main -->
--- resources/views/components/header.php
+++ resources/views/components/header.php
@@ -1,5 +1,66 @@
 <header>
-  <nav>
-    Navbar
+  <nav class="navbar navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="<?= __IMGDIR; ?>/header/logo.jpeg" alt="" width="100">
+      </a>
+      <a class="navbar-brand" href="#">
+        <img src="<?= __IMGDIR; ?>/header/parceiros.png" alt="" width="350">
+      </a>
+    </div>
+  </nav>
+
+  <nav class="navbar d-flex justify-content-end">
+    <div class="container justify-content-end p-2">
+      <a class="navbar-brand" href="#">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a class="navbar-brand" href="#">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a class="navbar-brand pe-5" href="#">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a class="navbar-brand pe-5" href="#">
+        <i class="far fa-file-alt"></i>
+       Protocolo
+      </a>
+
+      <a class="navbar-brand" href="#">
+        <i class="fas fa-user"></i>
+        Login/ Cadastro
+      </a>
+    </div>
+
+  </nav>
+
+
+  <nav class="navbar navbar-expand-lg">
+    <div class="menu container-fluid" style="background-color: #95C11F;">
+      <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+      <div class="container collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav  mb-2 mb-lg-0 p-2">
+          <li class="nav-item">
+            <a class="nav-link" style="color: #fff;" aria-current="page" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color: #fff;" aria-current="page" href="#">Quem Somos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color: #fff;" aria-current="page" href="#">Serviços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color: #fff;" aria-current="page" href="#">Comunicação</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color: #fff;" aria-current="page" href="#">Fale Conosco</a>
+          </li>
+        </ul>
+        <!-- <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form> -->
+      </div>
+    </div>
   </nav>
 </header><!-- header -->
