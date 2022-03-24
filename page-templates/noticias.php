dec7c7ae140022f1e8a074b78783ae90640e1d5e
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-03-23 23:23:24 -0300

    Ajuste de layout no template Notícias

--- page-templates/noticias.php
+++ page-templates/noticias.php
@@ -6,14 +6,53 @@
   get_header();
 ?>
 
-<main style="margin-top: 30px;">
+<main id="noticais" style="margin-top: 30px;">
   <div class="container">
     <h1>
       <?= get_the_title(); ?>
     </h1>
 
-    <br>
-    <?php the_content(); ?>
+    <?php 
+    // the query
+    $the_query = new WP_Query( array(
+      'category_name' => 'Notícias',
+      'posts_per_page' => 3,
+    )); 
+    ?>
+
+
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <?php if ( $the_query->have_posts() ) : ?>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    <div class="col-lg-4">
+      <div class="card" style="max-width: 300px">
+        <a href="<?= get_the_permalink(); ?>" title="<?= get_the_title(); ?>" class="d-flex justify-content-center" style="height: 200px;">
+          <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php echo the_title(); ?>"  class="card-img-top">
+        </a>
+        <div class="card-body p-0">
+          <h5 class="card-title pt-2">
+          <a style="color: black !important;" href="<?= get_the_permalink(); ?>">
+            <?php the_title(); ?>
+          </a>
+          </h5>
+
+          <?php the_excerpt(); ?>
+          
+        </div>
+      </div>
+    </div>
+
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+
+  <?php else : ?>
+    <p><?php __('No News'); ?></p>
+  <?php endif; ?>
+
+
+
   </div>
 </main>
 
