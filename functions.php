236e7b453b2db01d24a03662225b8f92fbc9cb24
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-03-23 23:24:33 -0300

    Ajuste para leia mais em functions

--- functions.php
+++ functions.php
@@ -28,7 +28,22 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', '0.0.1' );
 }
 
+function wpdocs_custom_excerpt_length( $length ) {
+	return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
 
+function wpdocs_excerpt_more( $more ) {
+	if ( ! is_single() ) {
+			$more = sprintf( '<a class="read-more" href="%1$s">%2$s</a>',
+					get_permalink( get_the_ID() ),
+					__( ' Leia Mais', 'textdomain' )
+			);
+	}
+
+	return $more;
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
 
 /**
